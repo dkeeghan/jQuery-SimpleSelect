74c91b8c16538106cd294fb7b573fc263e7b0daf
Damian Keeghan <dkeeghan@deloitte.com.au>
2014-02-04 11:10:02 +1100

    Cleaned up file, added copyright notice, changed .bind to .on, added ability to have multiple container classes and turn off sliding doors

--- js/jquery.simpleSelect-0.1.js
+++ js/jquery.simpleSelect-0.1.js
@@ -1,57 +1,76 @@
-(function($){
-    $.simpleSelect = function(el, options){
-        var base = this;
+/*!
+ * jQuery Simple Select 1.0
+ *
+ * https://github.com/dkeeghan/jQuery-SimpleSelect
+ *
+ * Copyright 2014 Damian Keeghan
+ * Released under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
 
-        base.$el = $(el);
-        base.el = el;
-		
+(function($){
+	$.simpleSelect = function(el, options){
+		var base = this;
+
+		base.$el = $(el);
+		base.el = el;
+
 		//if the simpleSelect code hasn't already run
 		if(typeof(base.$el.data('simpleSelect')) === 'undefined'){
-		
-	        base.$el.data('simpleSelect', base);
-			
-	        base.init = function(){
+
+			base.$el.data('simpleSelect', base);
+
+			base.init = function(){
 				// extend options
-	            base.options = $.extend({}, $.simpleSelect.defaultOptions, options);
-				
-				base.$el.wrap('<span class="'+base.options.containerClass+'"><span></span></span>').before('<span class="'+base.options.labelClass+'"></span>').css('opacity', 0);
-				
-				base.$container = base.$el.closest('.'+base.options.containerClass),
+				base.options = $.extend({}, $.simpleSelect.defaultOptions, options);
+
+				base.$el.wrap('<span class="'+base.options.containerClass+'"></span>').before('<span class="'+base.options.labelClass+'"></span>').css('opacity', 0);
+
+				base.options.containerClass = base.options.containerClass.replace(' ', '.');
+
+				base.$container = base.$el.closest('.'+base.options.containerClass);
 				base.$label = base.$container.find('.'+base.options.labelClass);
-				
+
+				if(base.options.slidingDoors){
+					base.$container.children().wrapAll('<span></span>');
+				}
+
+				base.$container.addClass(base.$el.attr('class'));
+
 				//bind keyboard/mouse actions
-				base.$el.bind('change keyup', function(){
+				base.$el.on('change keyup', function(){
 					base.$el.trigger('updateLabel.simpleSelect');
-				}).bind('focus', function(){
+				}).on('focus', function(){
 					base.$container.addClass(base.options.focusClass);
-				}).bind('blur', function(){
+				}).on('blur', function(){
 					base.$container.removeClass(base.options.focusClass);
-				}).bind('updateLabel.simpleSelect', function(){
+				}).on('updateLabel.simpleSelect', function(){
 					base.$label.text(base.$el.find('option:selected').text());
 				});
-				
+
 				//do initial label update
 				base.$el.trigger('updateLabel.simpleSelect');
-			
-			}
-			
+
+			};
+
 			//initialise
 			base.init();
-		
-		}
-	}
-	
-	//default options
-    $.simpleSelect.defaultOptions = {
-		containerClass: 'simpleSelect',
-		labelClass: 'simpleLabel',
-		focusClass: 'focus'			
-    };
 
-    $.fn.simpleSelect = function(options){
-        return this.each(function(){
-            (new $.simpleSelect(this, options));
-        });
-    };
+		}
+	};
+
+	//default options
+	$.simpleSelect.defaultOptions = {
+		containerClass: 'simple-select',
+		labelClass: 'simple-label',
+		focusClass: 'focus',
+		slidingDoors: true
+	};
+
+	$.fn.simpleSelect = function(options){
+		return this.each(function(){
+			(new $.simpleSelect(this, options));
+		});
+	};
 
 })(jQuery);
